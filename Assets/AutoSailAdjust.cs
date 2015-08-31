0d9bcd91edd31a338b8821dd328714da95654d1c
paleonluna <paleonluna@gmail.com>
2015-08-31 13:37:22 -0700

    first commit

---
+++ Assets/AutoSailAdjust.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class AutoSailAdjust : MonoBehaviour {
+
+	public Text angleWRTWindDebug,angleWRTWindDebug2;
+	public enum BoatSideFacingWind {Port, Starboard};
+	Vector3 directionWindComingFrom = new Vector3(0f,0f,1f);
+	float angleToAdjustTo;
+	float angleWRTWind;
+	Vector3 boatDirection;
+	public GameObject boat;
+	bool isJibing;
+	float smoothRate = 1f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//figure out angular relation ship between boat and wind
+		Vector3 localTarget = boat.transform.InverseTransformPoint(directionWindComingFrom);
+		//isNegative lets us know port vs starboard
+		float isNegative = Mathf.Atan2(localTarget.x, localTarget.z)/Mathf.Abs(Mathf.Atan2(localTarget.x, localTarget.z));
+		boatDirection = boat.transform.forward;
+		angleWRTWind = Vector3.Angle(boatDirection,directionWindComingFrom) * isNegative;
+		angleWRTWindDebug.text = "Angle WRT Wind: "+angleWRTWind.ToString();
+		if (float.IsNaN(angleWRTWind)) {
+			angleWRTWind=0;
+		}
+//		transform.localRotation = Quaternion.Euler(0,angleWRTWind/2,0);
+		transform.rotation = Quaternion.Lerp(Quaternion.identity, boat.transform.rotation, 0.5f);
+	}
+
+	void FixedUpdate () {
+
+	}
+}
---
+++ Assets/BoatControl.cs
@@ -0,0 +1,81 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+
+public class pointOfSail{
+	public string sailTitle;
+	public float angle;
+	public pointOfSail(string sailTitleToSet, float angleToSet){
+		angle = angleToSet;
+		sailTitle = sailTitleToSet;
+	}
+};
+public class BoatControl : MonoBehaviour {
+	public Text debug;
+	public TextAsset pointsOfSailTxt;
+	bool isLerpingAngle = false;
+	float lerpTime = .7f, lerpTimer, currAngle, endAngle;
+	int lastDirectionTurned = 0;
+	bool hasCurAngleBeenSet = false;
+	int indexPOS = 0;
+	int maxIndex;
+
+	List<pointOfSail> allPoints = new List<pointOfSail>();
+	
+	bool playerHasControl, pointsOfSailMode, allRangeMode;
+	// Use this for initialization
+	void Start () {
+		allPoints = TextParser.Parse(pointsOfSailTxt);
+		playerHasControl = true;
+		maxIndex = allPoints.Count - 1;
+	}
+
+	void StartLerp () {
+		//check if changing direction
+		isLerpingAngle = true;
+		lerpTimer = Time.time;
+	}
+	// Update is called once per frame
+	void Update () {
+		debug.text = allPoints[indexPOS].sailTitle;
+		if (isLerpingAngle) {
+			float fracJourney = (Time.time - lerpTimer)/lerpTime;
+			transform.rotation = Quaternion.Lerp (Quaternion.Euler(0, currAngle,0),Quaternion.Euler(0, allPoints[indexPOS].angle,0), fracJourney);
+
+			if (fracJourney > .99f) {
+				fracJourney = 1f;
+				transform.rotation = Quaternion.Lerp (Quaternion.Euler(0, currAngle,0),Quaternion.Euler(0, allPoints[indexPOS].angle,0), fracJourney);
+				isLerpingAngle = false;
+				currAngle = allPoints[indexPOS].angle;
+				
+			}
+
+		}
+
+		if (!isLerpingAngle && playerHasControl) {
+			hasCurAngleBeenSet = false;
+			if(Input.GetKeyDown(KeyCode.LeftArrow)) {
+				if(indexPOS == maxIndex) {
+					indexPOS = 0;
+				}
+				else {
+					indexPOS++;
+				}
+				StartLerp();
+			}	
+			if(Input.GetKeyDown(KeyCode.RightArrow)) {
+				if(indexPOS == 0) {
+					indexPOS = maxIndex;
+				}
+				else {
+					indexPOS--;
+				}
+				StartLerp();
+
+				
+			}
+
+		}
+	}
+}
