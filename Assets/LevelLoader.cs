bf914427e7963e00ad7ff3e38056f5fca1cf545c
paleonluna <paleonluna@gmail.com>
2015-09-25 14:00:01 -0700

    added intro scene that lets u choose of two levels

---
+++ Assets/LevelLoader.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelLoader : MonoBehaviour {
+
+	public void LoadLevel1() {
+		Application.LoadLevel("POSModule");
+
+	}
+
+	public void LoadLevel2() {
+		Application.LoadLevel("NavModule");
+	}
+}
