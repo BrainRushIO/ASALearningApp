66bc8474a51f5fd05cae6b7eacc62b8e9ea62bd4
paleonluna <paleonluna@gmail.com>
2015-09-29 12:25:41 -0700

    put in wind arrows

---
+++ Assets/WindArrow.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WindArrow : MonoBehaviour {
+	float spawnTimer;
+	// Use this for initialization
+	float spawnFrequency = 40f;
+	// Use this for initialization
+	void Start () {
+		spawnTimer = Time.time;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Translate(transform.forward*-.2f);
+		if (Time.time - spawnTimer > spawnFrequency) {
+			Destroy(gameObject);
+		}
+	}
+}
