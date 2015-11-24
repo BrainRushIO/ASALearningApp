4884faa116a1d2719e085541201547268bc804b7
paleonluna <paleonluna@gmail.com>
2015-11-23 17:26:35 -0800

    scene fixes

---
+++ Assets/TextFix1.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class TextFix1 : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		GetComponent<Text> ().text = "or " + "<color=#FFFFFF>click here </color>" +  "to skip to level 2";
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/TextFix2.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class TextFix2 : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		GetComponent<Text> ().text = "<color=#FFFFFF>click here </color>\n" +  "to learn the points of sail";
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
